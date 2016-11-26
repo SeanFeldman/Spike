2c939b285d5ef7e6eee91aad8e6e5b4d15285ffc
Sean Feldman <feldman.sean@gmail.com>
2016-11-25 17:45:36 -0700

    init

---
+++ TestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TestApp
+{
+    using System;
+    using Microsoft.ServiceBus.Messaging;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msg = new BrokeredMessage("x");
+            Console.WriteLine(msg.Size);
+        }
+    }
+}
